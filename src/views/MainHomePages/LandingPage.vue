47772e950eba9d4db8278c73d3db469db94d217c
tejaswip88 <tejaswi@mobil80.com>
2025-07-14 12:25:35 +0530

    MASTER 12:25pm

--- src/views/MainHomePages/LandingPage.vue
+++ src/views/MainHomePages/LandingPage.vue
@@ -312,21 +312,17 @@ export default {
     //   console.warn("StorageManager API not supported in this browser.");
     // }
 
-    // Uncomment After this
-    setTimeout(async () => {
-      const indexedDbDataObj = await getOrgS3DataMethod();
-      const orgDetails = this.$store.getters.get_current_user_details;
-      const currentOrgId = orgDetails.organization_id;
-      if (
-        !indexedDbDataObj ||
-        Object.keys(indexedDbDataObj).length === 0 ||
-        indexedDbDataObj.current_org_id !== currentOrgId
-      ) {
-        await fetchAndStoreOrgS3DataMethod(orgDetails);
-      } else {
-        console.log("HERE_ELSE");
-      }
-    }, 2000);
+    //Uncomment After this
+    // const indexedDbDataObj = await getOrgS3DataMethod();
+    // const orgDetails = this.$store.getters.get_current_user_details;
+    // const currentOrgId = orgDetails.organization_id;
+    // if (
+    //   !indexedDbDataObj ||
+    //   Object.keys(indexedDbDataObj).length === 0 ||
+    //   indexedDbDataObj.current_org_id !== currentOrgId
+    // ) {
+    //   await fetchAndStoreOrgS3DataMethod(orgDetails);
+    // }
   },
   methods: {
     toggleSubMenu(parentMenuName, subMenuName) {
